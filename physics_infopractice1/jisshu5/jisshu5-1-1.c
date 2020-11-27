768da379efcc5bcad9265b66be6b2812c1d8bf17
酒井葵生 <sakaiaoisei@sakaiaoiseinoMacBook-Air.local>
2020-11-27 09:37:57 +0900

    [add]実習5-1-2の追加

--- physics_infopractice1/jisshu5/jisshu5-1-1.c
+++ physics_infopractice1/jisshu5/jisshu5-1-1.c
@@ -2,21 +2,37 @@
 #include<math.h>
 
 int main(){
-    int N = 100;
+    int N = 100, M = 1000;
     double a[N+1];
     double b[N+1];
-    double a0 = 0.5, x = 0.25, sum = 0;
+    double a0 = 0.5, x[M+1], W[M+1];
     int b0 = 15;
+    for (int i = 0; i < M+1; i++)
+    {
+        W[i] = 0;
+    }
+    
     for (int i = 0; i < N+1; i++)
     {
         a[i] = pow(a0, i);
         b[i] = M_PI * pow(b0, i);
     }
-    for (int i = 0; i < N+1; i++)
+    for (int i = 0; i < M+1; i++)
     {
-        sum += a[i] * cos(b[i] * x);
+        x[i] = -2. + 4. * i / (double)M;
     }
-    printf("W(x=0.25) = %.15e\n", sum);
+    
+    for (int i = 0; i < M+1; i++)
+    {
+        for (int j = 0; j < N+1; j++)
+        {
+            W[i] += a[j] * cos(b[j] * x[i]);
+            
+        }
+        printf("%.15e %.15e\n", x[i], W[i]);
+    }
+    
+    
     
     return 0;
 }
