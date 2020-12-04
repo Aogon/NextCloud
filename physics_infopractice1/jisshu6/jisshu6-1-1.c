025cb8a5651a2267c8861e1789bfbf608f47149f
酒井葵生 <sakaiaoisei@sakaiaoiseinoMacBook-Air.local>
2020-12-04 10:51:51 +0900

    [add]実習6-1の追加

---
+++ physics_infopractice1/jisshu6/jisshu6-1-1.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int N=32, M=30000;
+    double p[N], p_new[N], q[N], q_new[N], alpha=1./4., dt=sqrt(1./8.), time=0.;
+    FILE *fp_q;
+    fp_q = fopen("q.dat", "w");
+    for (int i = 0; i < N; i++)
+    {
+        p[i] = 0.;
+        q[i] = sin(M_PI * i / (N - 1.));
+    }
+    for (int i = 0; i < M; i++)
+    {
+        time = time + dt;
+        q_new[0] = 0.;
+        p_new[0] = 0.;
+        for (int j = 1; j < N-1; j++)
+        {
+            p_new[j] = p[j] - dt * (-(q[j+1] + q[j-1] - 2. * q[j]) 
+                + alpha * ((q[j+1] - q[j]) * (q[j+1] - q[j]) - (q[j] - q[j-1]) * (q[j] - q[j-1])));
+            q_new[j] = q[j] + dt * p_new[j];
+        }
+        q_new[N-1] = 0.;
+        p_new[N-1] = 0.;
+        for (int j = 0; j < N; j++)
+        {
+            q[j] = q_new[j];
+            p[j] = p_new[j];
+        }
+        if (i % 3 == 0)
+        {
+            fprintf(fp_q, "# step %d \n", i);
+            for (int j = 0; j < N; j++)
+            {
+                fprintf(fp_q, "%d %e %e\n", j, q[j], p[j]);
+            }
+            fprintf(fp_q, "\n\n");
+        }
+        
+        
+        
+    }
+    
+    
+    return 0;
+}
